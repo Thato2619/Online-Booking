c42a72b521a9e170155d21cb12e3e0e177a43a6e
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-05 13:05:55 +0200

    Create FaceMask class and fields within class

---
+++ model/FaceMask.php
@@ -0,0 +1,16 @@
+<?php 
+//create FaceMask class 
+class FaceMask {
+    // fields
+    private $ID;
+    private $name;
+    private $description;
+    private $price;
+    private $image;
+    private $type;
+}
+
+
+
+
+?>
