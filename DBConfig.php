4cfc2319599cb691a25535e06cc8c4f523284506
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-05 12:04:38 +0200

    create DBConfig class.
    don't forget to comment

--- DBConfig.php
+++ DBConfig.php
@@ -1,6 +1,15 @@
 <?php 
 
+//create database class with user, password, db, host & port
 
+class DBConfig {
+    private $user = 'root';
+    private $password = 'root';
+    private $db = 'Online-Store';
+    private $host = 'localhost';
+    private $port = 8888;
+
+}
 
 
 
