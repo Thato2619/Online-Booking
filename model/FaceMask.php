b4c856023a659b312196e6488807156e8079d89b
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-05 17:04:32 +0200

    create getters and setters of the field names
    id
    name
    description
    price
    type
    availabilty

--- model/FaceMask.php
+++ model/FaceMask.php
@@ -47,6 +47,83 @@ class FaceMask {
             }
         }
     }
+
+    // getters and setters
+    #getter and setter for ID
+    public function getID() {
+        return $this->ID;
+    }
+    public function setID($ID){
+        $this->ID = $ID;
+
+        return $this;
+    }
+
+    #getter and setter for name
+    public function getName(){
+        return $this->name;
+    }
+    public function setName($name) {
+        return $this->name = $name;
+
+        return $this;
+    }
+    
+    #getter and setter for description
+    public function getDescription() {
+        return $this->description;
+    }
+    public function setDescription($description) {
+        return $this->description = $description;
+
+        return $this;
+    }
+    
+    #getter and setter for price
+    public function getPrice() {
+        return $this->price;
+    }
+    public function setPrice($price) {
+        return $this->price = $price;
+
+        return $this;
+    }
+
+    #getter and setter for image
+    public function getImage() {
+        return $this->image;
+    }
+    public function setImage($image) {
+        return $this->image = $image;
+
+        return $this;
+    }
+    
+
+    #getter and setter for type
+    public function getType() {
+        return $this->type;
+    }
+    public function setType($type) {
+        return $this->type = $type;
+
+        return $this;
+    }
+
+    #getter and setter for availabilty
+    public function getAvailability() {
+        return $this->availability;
+    }
+    public function setAvailabilty($availability) {
+        return $this->availability = $availability;
+
+        return $this;
+    }
+    
+
+
+
+    
 }
 
 
