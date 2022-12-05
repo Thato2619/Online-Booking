68d080b17920cc866a99b6865797da998f6bd6df
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-05 12:15:18 +0200

    create method that help connects to database in local code

--- DBConfig.php
+++ DBConfig.php
@@ -9,6 +9,27 @@ class DBConfig {
     private $host = 'localhost';
     private $port = 8888;
 
+    //create database method to connect to database via local code
+    public function connectToDatabse(){
+        $mysqli = new \mysqli(
+            $this->host,
+            $this->user,
+            $this->password,
+            $this->db,
+            $this->port
+        );
+
+        //check if connection is successful
+        if($mysqli->connect_error) {
+            die("connection:failed: " . $mysqli->connect_error);
+
+        } else {
+            // echo out, "connected sucessfully"
+            return $mysqli;
+        }
+    }
+    
+
 }
 
 
