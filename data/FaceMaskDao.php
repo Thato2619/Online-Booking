e17300b614348086145115bb114b1f9aeb36087b
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-08 01:39:18 +0200

    send in request to connect to database by using conditionals
    -use die function , in case for connection error
    -create result variable to send in query

--- data/FaceMaskDao.php
+++ data/FaceMaskDao.php
@@ -7,9 +7,10 @@ require_once __DIR__ . "/../model/FaceMask.php";
 class FaceMaskDao {
     //create facemask products
     public function createFaceMask($DBConfig, $FaceMask) {
+
         //add connection bewteen database and facemask product
         $connect = $DBConfig->connectToDatabase();
-
+        
         //see SQL statement
         $sqlStmt = "INSERT INTO FaceMask_products (ID, Name, Description, Price, Image, Availability)" . "VALUES(
             '".$FaceMask->getID()."',
@@ -17,11 +18,25 @@ class FaceMaskDao {
             '".$FaceMask->getDescription()."',
             '".$FaceMask->getPrice()."',
             '".$FaceMask->getImage()."',
-            '".$FaceMask->getAvailability()."',
-        );
+            '".$FaceMask->getAvailability()."')";
 
+        //send in request 
+        if($result = $connect->query($sqlStmt)) {
+
+            $connect->close();
+            return $result; //whether the result returns true or false
+        } else {
+            //use die funcion to kill any connection that may result to error in database
+            die("connection error:" . $connect->error);
+        }
     }
+    
+    
+
 }
+        
+
+
 
 
 
