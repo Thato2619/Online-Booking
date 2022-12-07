9614065f5ef3fd499b62126cad709c36a01457ae
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-08 01:22:24 +0200

    create connection to database and create sql statement
    create sqlstmt by inserting all of the fields of the FaceMask products

--- data/FaceMaskDao.php
+++ data/FaceMaskDao.php
@@ -1,11 +1,26 @@
 <?php 
-
+//include files from model and config folder
 require_once __DIR__ . "/../model/FaceMask.php";
-require_once __DIR__ . "/../config/DBConfig.php";
+#require_once __DIR__ . "/../config/DBConfig.php";
 
 
 class FaceMaskDao {
-    
+    //create facemask products
+    public function createFaceMask($DBConfig, $FaceMask) {
+        //add connection bewteen database and facemask product
+        $connect = $DBConfig->connectToDatabase();
+
+        //see SQL statement
+        $sqlStmt = "INSERT INTO FaceMask_products (ID, Name, Description, Price, Image, Availability)" . "VALUES(
+            '".$FaceMask->getID()."',
+            '".$FaceMask->getName()."',
+            '".$FaceMask->getDescription()."',
+            '".$FaceMask->getPrice()."',
+            '".$FaceMask->getImage()."',
+            '".$FaceMask->getAvailability()."',
+        );
+
+    }
 }
 
 
