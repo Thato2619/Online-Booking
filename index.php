fc2e296095681cdbbc43c89d20ff2140e77e9723
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-09 02:20:20 +0200

    include required files and start  sessions

--- index.php
+++ index.php
@@ -5,8 +5,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+#add sessiion and include files and sessions
+require_once __DIR__ . "/config/DBConfig.php";
+require_once __DIR__ . "/data/FaceMaskDao.php";
+require_once __DIR__ . "/model/FaceMask.php";
 
-
+session_start();
 
 
 
