bbf854e22344a79ed7ed2db51d404a5fb9096b3c
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-09 23:29:00 +0200

    remove everything from boilerplate in index.php and styes.css

--- index.php
+++ index.php
@@ -34,67 +34,7 @@ if (isset($_SESSION['outOfStock']) && $_SESSION['outOfStock'] == true) {
 }
 ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link rel="stylesheet" href="static/CSS/styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  </head>
-  <body>
 
-
-<!-- NAVBAR --->
-<nav class="navbar navbar-expand-lg bg-light py-3 fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">GLISTENING GLOW</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse navButton" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Gallery</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Shop</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact Us</a>
-        </li>
-        <li class="nav-item">
-             <i class="fas fa-shopping-bag"></i>
-             <i class="fas fa-user"></i>
-        </li>
-    </div>
-  </div>
-</nav>
-
-<!-------- Banner/hero--------->
-<section class="main">
-<div class="container">
-    <div class="row">
-        <div class="col-log-6">
-
-        
-        </div>
-    </div>
-</div>
-</section>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
-  </body>
-</html>
 
 <!-- all credits from images from pexel
 Photo by Anna Nekrashevich: https://www.pexels.com/photo/close-up-shot-of-cosmetic-products-8533265/
