d3e8fd1224fd78e2b2ac44eab77be0c039d36575
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-09 19:49:57 +0200

    add section tags and 3 div tags for banner and hero of page

--- index.php
+++ index.php
@@ -74,16 +74,22 @@ if (isset($_SESSION['outOfStock']) && $_SESSION['outOfStock'] == true) {
         <li class="nav-item">
              <i class="fas fa-shopping-bag"></i>
              <i class="fas fa-user"></i>
-
         </li>
-    
-      <!--<form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
-</nav>-->
+</nav>
+
+<!-------- Banner/hero--------->
+<section class="main">
+<div class="container">
+    <div class="row">
+        <div class="col-log-6">
+
+        
+        </div>
+    </div>
+</div>
+</section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
