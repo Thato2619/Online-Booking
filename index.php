bb2c5c1eb04e905b911188ef081ebcae23b50505
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-09 17:04:59 +0200

    create new class in div of nav collapse called navButton

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if (isset($_SESSION['outOfStock']) && $_SESSION['outOfStock'] == true) {
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse navButton" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         
         <li class="nav-item">
