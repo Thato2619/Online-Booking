ae206d3bd11be8578ec8fd452f72f604c916f61f
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-09 23:37:44 +0200

    add tailwind script tags using cdn

--- index.php
+++ index.php
@@ -34,7 +34,19 @@ if (isset($_SESSION['outOfStock']) && $_SESSION['outOfStock'] == true) {
 }
 ?>
 
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+</body>
+</html>
 
 <!-- all credits from images from pexel
 Photo by Anna Nekrashevich: https://www.pexels.com/photo/close-up-shot-of-cosmetic-products-8533265/
