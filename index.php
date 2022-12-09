4d54d6bfb8b024be51373e1e8f998f37c175a2f3
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-09 02:40:00 +0200

    change title to Glistening-Glow

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ if (isset($_SESSION['outOfStock']) && $_SESSION['outOfStock'] == true) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Glistening-Glow</title>
 </head>
 <body>
     
