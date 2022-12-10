614e60679b19eb15fffc49345188432dffbb2d11
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-10 18:47:12 +0200

    remove bootstrap and create custom cards

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ require_once __DIR__ . "/config/DBConfig.php";
 require_once __DIR__ . "/data/FaceMaskDao.php";
 require_once __DIR__ . "/model/FaceMask.php";
 
+
 session_start();
 
 #  add FaceMaskDao and Database objects
@@ -34,266 +35,22 @@ if (isset($_SESSION['outOfStock']) && $_SESSION['outOfStock'] == true) {
 }
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
-        
-    <title>Document</title>
-</head>
-<body>
-    <!----- Navbar------>
-    <nav class="
-  relative
-  w-full
-  flex flex-wrap
-  items-center
-  justify-between
-  py-4
-  bg-gray-100
-  text-gray-500
-  hover:text-gray-700
-  focus:text-gray-700
-  shadow-lg
-  navbar navbar-expand-lg navbar-light
-  ">
-  <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-  <button class="
-      navbar-toggler
-      text-gray-500
-      border-0
-      hover:shadow-none hover:no-underline
-      py-2
-      px-2.5
-      bg-transparent
-      focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline
-    " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars"
-    class="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-    <path fill="currentColor"
-      d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
-    </path>
-  </svg>
-  </button>
-  <div class="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent">
-  <a class="
-        flex
-        items-center
-        text-gray-900
-        hover:text-gray-900
-        focus:text-gray-900
-        mt-2
-        lg:mt-0
-        mr-1
-      " href="#">
-    <img src="/static/Images/online_store_logo.png" style="height: 15px" alt=""
-      loading="lazy" />
-  </a>
-  <!-- Left links -->
-  <ul class="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
-    <li class="nav-item p-2">
-      <a class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="#">Home</a>
-    </li>
-    <li class="nav-item p-2">
-      <a class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="#">Gallery<a>
-    </li>
-    <li class="nav-item p-2">
-      <a class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="#">Shop</a>
-    </li>
-    <li class="nav-item p-2">
-      <a class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="#">About Us</a>
-    </li>
-    <li class="nav-item p-2">
-      <a class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" href="#">Contact us</a>
-    </li>
-  </ul>
-  <!-- Left links -->
-  </div>
-  <!-- Collapsible wrapper -->
-
-  <!-- Right elements -->
-  <div class="flex items-center relative">
-  <!-- Icon -->
-  <a class="text-gray-500 hover:text-gray-700 focus:text-gray-700 mr-4" href="#">
-    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="shopping-cart"
-      class="w-4" role="img" xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 576 512">
-      <path fill="currentColor"
-        d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z">
-      </path>
-    </svg>
-  </a>
-  <div class="dropdown relative">
-    <a class="
-          text-gray-500
-          hover:text-gray-700
-          focus:text-gray-700
-          mr-4
-          dropdown-toggle
-          hidden-arrow
-          flex items-center
-        " href="#" id="dropdownMenuButton1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bell"
-        class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-        <path fill="currentColor"
-        </path>
-      </svg>
-    </a>
-    <ul class="
-      dropdown-menu
-      min-w-max
-      absolute
-      hidden
-      bg-white
-      text-base
-      z-50
-      float-left
-      py-2
-      list-none
-      text-left
-      rounded-lg
-      shadow-lg
-      mt-1
-      hidden
-      m-0
-      bg-clip-padding
-      border-none
-      left-auto
-      right-0
-    " aria-labelledby="dropdownMenuButton1">
-      <li>
-        <a class="
-          dropdown-item
-          text-sm
-          py-2
-          px-4
-          font-normal
-          block
-          w-full
-          whitespace-nowrap
-          bg-transparent
-          text-gray-700
-          hover:bg-gray-100
-        " href="#">Action</a>
-      </li>
-      <li>
-        <a class="
-          dropdown-item
-          text-sm
-          py-2
-          px-4
-          font-normal
-          block
-          w-full
-          whitespace-nowrap
-          bg-transparent
-          text-gray-700
-          hover:bg-gray-100
-        " href="#">Another action</a>
-      </li>
-      <li>
-        <a class="
-          dropdown-item
-          text-sm
-          py-2
-          px-4
-          font-normal
-          block
-          w-full
-          whitespace-nowrap
-          bg-transparent
-          text-gray-700
-          hover:bg-gray-100
-        " href="#">Something else here</a>
-      </li>
-    </ul>
-  </div>
-  <div class="dropdown relative">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Glistening Glow</title>
    
-    <ul class="
-    dropdown-menu
-    min-w-max
-    absolute
-    hidden
-    bg-white
-    text-base
-    z-50
-    float-left
-    py-2
-    list-none
-    text-left
-    rounded-lg
-    shadow-lg
-    mt-1
-    hidden
-    m-0
-    bg-clip-padding
-    border-none
-    left-auto
-    right-0
-  " aria-labelledby="dropdownMenuButton2">
-      <li>
-        <a class="
-        dropdown-item
-        text-sm
-        py-2
-        px-4
-        font-normal
-        block
-        w-full
-        whitespace-nowrap
-        bg-transparent
-        text-gray-700
-        hover:bg-gray-100
-      " href="#">Action</a>
-      </li>
-      <li>
-        <a class="
-        dropdown-item
-        text-sm
-        py-2
-        px-4
-        font-normal
-        block
-        w-full
-        whitespace-nowrap
-        bg-transparent
-        text-gray-700
-        hover:bg-gray-100
-      " href="#">Another action</a>
-      </li>
-      <li>
-        <a class="
-        dropdown-item
-        text-sm
-        py-2
-        px-4
-        font-normal
-        block
-        w-full
-        whitespace-nowrap
-        bg-transparent
-        text-gray-700
-        hover:bg-gray-100
-      " href="#">Something else here</a>
-      </li>
-    </ul>
-  </div>
-  </div>
-  <!-- Right elements -->
-  </div>
-</nav>
-
-<script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
-</body>
+    <h1>Glistening Glow</h1>
+    <div></div>
+  </head>
+  <body>
+   
+  </body>
 </html>
-
 <!-- all credits from images from pexel
 Photo by Anna Nekrashevich: https://www.pexels.com/photo/close-up-shot-of-cosmetic-products-8533265/
+Photo by Angela Roma : https://www.pexels.com/photo/gentle-black-woman-applying-cream-on-face-7479816/
  -->
