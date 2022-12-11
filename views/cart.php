ee915bc4abe744a443f09bc508a96bee5e514d56
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-12-11 22:13:01 +0200

    create assocate array that id, name,price,quantity

--- views/cart.php
+++ views/cart.php
@@ -7,6 +7,23 @@
 
     session_start();
     
+    $connect = mysqli_connect("localhost", "root", "online_store");
+    //create session for add_cart and cart
+    if(isset($_POST['add_to_cart'])) {
+        if(isset($_SESSION['cart'])) {
+            $session_aray_name = array_column($_SESSION['cart'], "name");
+        }else{
+            $session_array = array(
+                "id" => $_GET['id'],
+                "name" => $_POST['name'],
+                "price" => $_POST['price'],
+                "quantity" => $_POST['quantity'],
+
+            );
+        }
+    
+
+    }
     //create cart session to preview
     //initialise cart if not set 
     if(!isset($_SESSION['cart'])) {
